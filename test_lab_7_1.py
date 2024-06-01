30258f53c53b3ed49c09ea9a24db86266ce3f70c
VladimirPutinHero <160325494+VladimirPutinHero@users.noreply.github.com>
2024-06-01 09:30:20 +0300

    Update test_lab_7_1.py

--- test_lab_7_1.py
+++ test_lab_7_1.py
@@ -4,7 +4,7 @@ from lab_7_1 import count_ways_to_climb
 
 class TestCountWaysToClimb(unittest.TestCase):
     def test_count_ways_to_climb(self):
-        self.assertEqual(count_ways_to_climb(4), 8)
+        self.assertEqual(count_ways_to_climb(4), 5)
         self.assertEqual(count_ways_to_climb(8), 34)
 
 
